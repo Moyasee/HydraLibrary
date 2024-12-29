d48b718c14b8a281d6a1b09819e417b6263cce9b
zxcsix-zxc <terranoiz@icloud.com>
2024-12-30 00:16:34 +0200

    fixing the loading firebase database error

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { resolve } from 'path';
 
-export default defineConfig({
-  base: '/HydraLibrary/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        about: resolve(__dirname, 'about.html')
-      },
-      output: {
-        manualChunks: {
-          firebase: ['firebase/app', 'firebase/database']
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    base: '/HydraLibrary/',
+    build: {
+      outDir: 'dist',
+      assetsDir: 'assets',
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          about: resolve(__dirname, 'about.html')
         }
       }
+    },
+    define: {
+      'process.env': env
     }
-  }
+  };
 }); 
