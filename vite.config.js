535e88f59ec078bcb29ebcfe9ed2630453f0f454
zxcsix-zxc <terranoiz@icloud.com>
2025-05-26 11:38:57 +0300

    minor changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/HydraLibrary/',
+  base: process.env.VITE_BASE_PATH || '/',
   server: {
     port: 3000,
     open: true
