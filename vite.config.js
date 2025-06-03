ac9e1b29329c37266d879e3c07dfd7ddc7d55c07
zxcsix-zxc <terranoiz@icloud.com>
2025-06-03 18:45:30 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,6 @@ export default defineConfig({
         main: 'index.html',
         library: 'library.html',
         about: 'about.html',
-        donate: 'donate.html',
         changelog: 'changelog.html'
       }
     }
